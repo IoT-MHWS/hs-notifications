587e0f7957210a60ac1bf1789daa2f3a07e4baf0
thermvs <mariagewitter@gmail.com>
2023-12-28 02:48:17 +0300

    feat: init project

---
+++ src/main/java/artgallery/hsnotifications/HsNotificationsApplication.java
@@ -0,0 +1,13 @@
+package artgallery.hsnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsNotificationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/artgallery/hsnotifications/HsNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package artgallery.hsnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
